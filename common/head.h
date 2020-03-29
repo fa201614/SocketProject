f792c998b766e6f8f8bf80b2dd18a13b9706676a
fa201614 <1870642326@qq.com>
2020-03-29 13:35:05 +0800

    v0.1

---
+++ common/head.h
@@ -0,0 +1,10 @@
+#ifndef HEAD_H
+#define HEAD_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <sys/wait.h>
+#endif
---
+++ server/server.c
@@ -0,0 +1,19 @@
+#include "head.h"
+#include "tcp_server.h"
+
+int main(int argc, char **argv) {
+    int port, server_listen;
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s port!\n", argv[0]);
+        return 1;
+    }
+    port = atoi(argv[1]);
+
+    if ((server_listen = socket_create(port)) < 0) {
+        perror("socket_create");
+        return 2;
+    }
+
+    return 0;
+}
+
